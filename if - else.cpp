6e166913b7a889b001a3448ac6f2a5193ae5f3ac
harshal-jadhav001 <dwaraknath.harshal24@sanjivani.edu.in>
2024-08-25 00:20:36 +0530

    Add files via upload

---
+++ if - else.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+ int age;
+ 
+ cout <<"Enter you age:";
+ cin >> age;
+ 
+ if (age >= 18){
+    cout << "you are an adult." <<endl;
+ } else {
+    cout << "you are not an adult." <<endl;
+    
+ }
+    return 0;
+}
