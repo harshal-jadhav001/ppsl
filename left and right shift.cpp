19724b36602cce921b60bee61eee86e54428ce40
harshal-jadhav001 <dwaraknath.harshal24@sanjivani.edu.in>
2024-08-25 00:20:52 +0530

    Add files via upload

---
+++ left and right shift.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+  
+  int a=2, b=4;
+  
+  // Binary ANd opertor 
+  cout <<"a & b is" << (a & b) << endl;
+  
+  // Binary OR operator 
+  cout << "a | b is" << (a | b) << endl;
+  
+  // Binari XOR operator 
+  cout <<"a ^ b is" << (a^b) << endl;
+  
+  // Left Shift operator 
+  cout <<"a<<1 is" <<(a << 1) << endl;
+   
+   // Right Shift operator 
+   cout << "a>>1 is" << (a << 1) << endl;
+   
+   // Right Shift opertor 
+    cout << "a>>1 is" << (a >> 1) << endl;
+    
+    // One's Complement operator 
+    cout << "-(a) is" <<-(a) << endl;
+  
+    return 0;
+}
