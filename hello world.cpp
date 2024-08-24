357197cf721687f2952748e7608950287974b620
harshal-jadhav001 <dwaraknath.harshal24@sanjivani.edu.in>
2024-08-25 00:19:22 +0530

    Add files via upload

---
+++ hello world.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+// Main ()function : Where the execution of 
+ 
+ // program begins
+ int main()
+ {
+ 
+          // print hello world 
+          cout<<"Hello world";
+          
+return 0;
+}
