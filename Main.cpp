c300b6490b81b8f95ee4d7007e162f35e828ac8d
harshal-jadhav001 <dwaraknath.harshal24@sanjivani.edu.in>
2024-08-25 00:18:39 +0530

    Add files via upload

---
+++ Main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int a=3,b=4;
+  //conditional operator
+  int result=(a<b)?b:a;
+  cout<<"The greater number is "<< result <<endl;
+  
+    return 0;
+}
